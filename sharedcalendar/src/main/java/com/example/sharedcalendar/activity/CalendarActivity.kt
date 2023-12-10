b40d902b676a369545b4672c108167e4476f8058
stomimh <stomimh@gmail.com>
2023-12-10 16:56:16 +0900

    test

--- sharedcalendar/src/main/java/com/example/sharedcalendar/activity/CalendarActivity.kt
+++ sharedcalendar/src/main/java/com/example/sharedcalendar/activity/CalendarActivity.kt
@@ -23,7 +23,7 @@ import com.example.mychartviewlibrary.calendar.data.CalendarFilter
 import com.example.mychartviewlibrary.calendar.list.ITask
 import com.example.mychartviewlibrary.calendar.list.OnScheduleItemClickListener
 
-
+// comment for test.
 class CalendarActivity : AppCompatActivity() {
 
     private lateinit var mContext: Context
