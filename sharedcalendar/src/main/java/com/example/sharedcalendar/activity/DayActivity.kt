d3e9e5f7871585f4d8f0b9f0b49cc4339f52f764
kongyi <kongyi123@nate.com>
2023-12-16 20:55:09 +0900

    aa

--- sharedcalendar/src/main/java/com/example/sharedcalendar/activity/DayActivity.kt
+++ sharedcalendar/src/main/java/com/example/sharedcalendar/activity/DayActivity.kt
@@ -10,10 +10,6 @@ import com.example.common.data.Schedule
 import com.example.sharedcalendar.R
 
 
-
-
-
-
 class DayActivity : AppCompatActivity() {
     private lateinit var inputButtonView: Button
     private lateinit var closeButtonView: Button
