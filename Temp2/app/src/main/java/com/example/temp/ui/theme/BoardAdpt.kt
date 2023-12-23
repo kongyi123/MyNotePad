1fde260d579c557f5f5ad025bf8d75139a721e06
stomimh <stomimh@gmail.com>
2023-12-23 16:50:12 +0900

    캘린더 수정 샘플

---
+++ Temp2/app/src/main/java/com/example/temp/ui/theme/BoardAdpt.kt
@@ -0,0 +1,32 @@
+package com.example.temp.ui.theme
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.temp.R
+import java.util.Calendar
+
+
+class BoardAdpt(var context:Context, var Data:ArrayList<Day>): RecyclerView.Adapter<BoardAdpt.BoardViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.ltem, parent, false)
+        return BoardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BoardViewHolder, position: Int) {
+
+        holder.tv_time.text = Data[position].day
+    }
+    override fun getItemCount(): Int {
+        return Data.count()
+    }
+    inner class BoardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv_time = itemView.findViewById<TextView>(R.id.tv_time)
+
+    }
+}
+
---
+++ Temp2/app/src/main/java/com/example/temp/ui/theme/data.kt
@@ -0,0 +1,5 @@
+package com.example.temp.ui.theme
+
+data class Day(val day: String,var Year:Int=0,var Month:Int=0)
+
+
